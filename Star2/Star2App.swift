95296b8d144157a4aa47f9b3bae4ecf178556b72
Mahaaldajanii <maha050002@gmail.com>
2022-02-02 23:28:23 +0300

    Initial Commit

---
+++ Star2/Star2App.swift
@@ -0,0 +1,17 @@
+//
+//  Star2App.swift
+//  Star2
+//
+//  Created by Maha on 01/07/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct Star2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
